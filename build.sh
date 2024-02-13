dcc285902dbc9c23a45a83cf2d7590b683a58281
Hiroaki Masuzawa <masuzawa.hiroaki.vl@tut.jp>
2024-02-13 16:09:59 +0900

    chg to superbuild type

--- build.sh
+++ build.sh
@@ -1,1 +1,4 @@
-docker build -t isl_mc_rtc .
+docker build . -f Dockerfile.superbuild \
+    --build-arg BASE_IMAGE=repo.irsl.eiiris.tut.ac.jp/irsl_system:noetic \
+    --build-arg CATKIN_SETUP_FILE=/choreonoid_ws/install/setup.bash \
+    -t irsl_system_mcrtc:noetic
