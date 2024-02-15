999f4ac5c9c9ded7201ec85cd033bf97e749a70c
Hiroaki Masuzawa <masuzawa.hiroaki.vl@tut.jp>
2024-02-15 19:56:31 +0900

    wip

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
-docker build . -f Dockerfile.superbuild \
+DOCKER_BUILDKIT=1 docker build . -f Dockerfile.superbuild \
     --build-arg BASE_IMAGE=repo.irsl.eiiris.tut.ac.jp/irsl_system:noetic \
     --build-arg CATKIN_SETUP_FILE=/choreonoid_ws/install/setup.bash \
-    -t irsl_system_mcrtc:noetic
+    -t irsl_system_mcrtc:noetic \
+    --ssh default=$SSH_AUTH_SOCK
