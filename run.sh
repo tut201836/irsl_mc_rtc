3d935de1015e89cc13f638feeac31a94e65e0ab4
Hiroaki Masuzawa <masuzawa.hiroaki.vl@tut.jp>
2024-05-13 11:04:15 +0900

    wip

--- run.sh
+++ run.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-iname=${DOCKER_IMAGE:-"irsl_mc_rtc:ubuntu20.04"} ##
-cname=${DOCKER_CONTAINER:-"irsl_mc_rtc"} ## name of container (should be same as in exec.sh)
+iname="irsl_mc_rtc:ubuntu20.04" ##
+cname="irsl_mc_rtc" ## name of container (should be same as in exec.sh)
 robot=${ROBOT:-"JVRC1"} # JVRC1 or CHIDORI
-MC_RTC_WORKSPCE=/mc_rtc_ws
 
 if [ $robot = "JVRC1" ]; then
     MC_CONTROL_CONFIG_MOUNT="--volume=`pwd`/config/JVRC1/mc_rtc.yaml:/root/.config/mc_rtc/mc_rtc.yaml"
 elif [ $robot = "CHIDORI" ]; then
     MC_CONTROL_CONFIG_MOUNT="--volume=`pwd`/config/CHIDORI/mc_rtc.yaml:/root/.config/mc_rtc/mc_rtc.yaml"
+else 
+    echo "$ROBOT is not defined."
+    exit
 fi
 BWC_WORK_MOUNT="--volume=`pwd`/bwc_ws:/bwc_ws"
 
@@ -17,4 +19,4 @@ BWC_WORK_MOUNT="--volume=`pwd`/bwc_ws:/bwc_ws"
 --name $cname \
 --mount ${MC_CONTROL_CONFIG_MOUNT} \
 --mount ${BWC_WORK_MOUNT} \
---workspace "`pwd`/userdir"
+--workspace "`pwd`/userdir" "$@"
